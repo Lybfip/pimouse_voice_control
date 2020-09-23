aa218583237e9e9a0ffcc3d520466db68acc40d4
Lybfip Ruxeacrh <ct9a5820@gmail.com>
2020-09-23 04:23:42 +0000

    Update Travis test files

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,12 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_voice_control/
+rsync -av ./ ~/catkin_ws/src/pimouse_voice_control/     # change
+
+#pimouse_ros
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/Lybfip/pimouse_ros.git
+
+
 cd ~/catkin_ws
 catkin_make
